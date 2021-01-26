af169a528c1b1034843923498160cb0accfa730b
Dominik Bartkowski <dominikbartkowski@gmail.com>
2021-01-26 13:29:38 +0000

    -

--- scrasync/config/appconf.py
+++ scrasync/config/appconf.py
@@ -43,3 +43,26 @@ AIOHTTP_BUFSIZE = 8 * 1024
 REDIS_PASS = os.environ.get('REDIS_PASS')
 REDIS_DB_NUMBER = 0
 REDIS_PORT = 6379
+
+
+# RabbitMQ configuration
+# RabbitMQ rpc queue name
+# These values are defined on the level of docker-compose.
+RPC_QUEUE_NAME = os.environ.get('RPC_QUEUE_NAME', 'scrasync')
+
+# login credentials for RabbitMQ.
+RPC_PASS = os.environ.get('RABBITMQ_DEFAULT_PASS')
+RPC_USER = os.environ.get('RABBITMQ_DEFAULT_USER')
+RPC_VHOST = os.environ.get('RABBITMQ_DEFAULT_VHOST')
+
+# the host to which the rpc broker (rabbitmq) is deployed
+RPC_HOST = os.environ.get('RABBITMQ_HOST')
+RPC_PORT = os.environ.get('RABBITMQ_PORT', 5672)
+
+# mongodb celery backend
+DATABASE_USERNAME = os.environ.get('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
+MONGO_PORT = os.environ.get('MONGO_PORT')
+MONGODB_LOCATION = os.environ.get('MONGODB_LOCATION')
+RPC_DATABASE = os.environ.get('RPC_DATABASE')
+RPC_COLLECTION = os.environ.get('RPC_COLLECTION')
--- scrasync/config/celeryconf.py
+++ scrasync/config/celeryconf.py
@@ -1,11 +1,31 @@
 
-from .appconf import BROKER_HOST_NAME, REDIS_PASS
+# from .appconf import BROKER_HOST_NAME, REDIS_PASS
+
+# rabbitmq related imports
+from .appconf import RPC_HOST, RPC_PASS, RPC_PORT, RPC_USER, RPC_VHOST
+# celery backend related imports
+from .appconf import (DATABASE_USERNAME, DATABASE_PASSWORD, MONGODB_LOCATION,
+                      RPC_DATABASE)
 
 
-_url = f'redis://:{REDIS_PASS}@{BROKER_HOST_NAME}:6379/0'
+# broker_url = 'amqp://myuser:mypassword@localhost:5672/myvhost'
+_url = f'amqp://{RPC_USER}:{RPC_PASS}@{RPC_HOST}:{RPC_PORT}/{RPC_VHOST}'
+
+# _url = f'redis://:{REDIS_PASS}@{BROKER_HOST_NAME}:6379/0'
 
 BROKER_URL = _url
-CELERY_RESULT_BACKEND = _url
+# CELERY_RESULT_BACKEND = _url
+# CELERY_RESULT_BACKEND = 'rpc://'
+
+CELERY_RESULT_BACKEND = 'mongodb://'
+CELERY_MONGODB_BACKEND_SETTINGS = {
+    'host': MONGODB_LOCATION,
+    'user': DATABASE_USERNAME,
+    'password': DATABASE_PASSWORD,
+    'database_name': RPC_DATABASE
+}
+
+CELERY_RESULT_PERSISTENT = True
 
 
 CELERY_IMPORTS = ('scrasync.tasks', 'scrasync.scraper', )
